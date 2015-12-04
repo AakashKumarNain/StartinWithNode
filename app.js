b17ae8365f2197f82a5000e468acdac2211385e3
Aakash Kumar Nain <aakashnain@outlook.com>
2015-12-05 02:40:32 +0530

    Added the default view engine

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/', function(req,res){
         
-        res.send("<h1>Hello</h1> World");        
+        res.render('default' , {
+        	title : 'Home',
+        	classname : 'home',
+        	users : ['Aakash','Vishal','Megha','Daksh']
+        });        
 });
+
+app.get('/about', function(req,res){
+        
+        res.render('default' , {
+        	title : 'About us',
+        	classname : 'about'
+        });        
+});
+
+app.get('*',function(req,res)
+	{
+		res.send('<b>Sorry,No such page found<b/>');
+	});
 
 var server = app.listen(3000,function(){
         	console.log('Listening on port 3000');
