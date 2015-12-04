959b38a0370aeb5b85ac2cca82fd008b88a28bde
Aakash Kumar Nain <aakashnain@outlook.com>
2015-12-05 03:02:32 +0530

    Added routes

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 var express = require('express');
 var app = express();
-
+var routes = require('./routes');
 app.set('view engine','ejs');
 
-app.get('/', function(req,res){
-        
-        res.render('default' , {
-        	title : 'Home',
-        	classname : 'home',
-        	users : ['Aakash','Vishal','Megha','Daksh']
-        });        
-});
 
-app.get('/about', function(req,res){
-        
-        res.render('default' , {
-        	title : 'About us',
-        	classname : 'about'
-        });        
-});
+
+app.get('/', routes.index);
+app.get('/about', routes.about);
 
 app.get('*',function(req,res)
 	{
-		res.send('<b>Sorry,No such page found<b/>');
+		res.send('<b>Sorry!!No such page found<b/>');
 	});
 
 var server = app.listen(3000,function(){
