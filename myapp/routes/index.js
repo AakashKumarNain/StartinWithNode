7ec2d4c2e98449483b9f7468d522a8cfb8ab595f
Aakash Kumar Nain <aakashnain@outlook.com>
2015-12-06 23:05:45 +0530

    Final version of the website

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,12 +5,52 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res,next) {
   var myArtWork = [];
+  var myArtists = [];
+  myArtists = appdata.speakers;
+
   appdata.speakers.forEach(function(item){
     myArtWork = myArtWork.concat(item.artwork);
   });
   res.render('index', {
       title: 'Home',
-      artwork : myArtWork
+      artwork : myArtWork,
+      artists : myArtists,
+      page : 'home'
+  });
+});
+
+
+router.get('/speakers', function(req, res,next) {
+  var myArtWork = [];
+  var myArtists = [];
+  myArtists = appdata.speakers;
+  appdata.speakers.forEach(function(item){
+    myArtWork = myArtWork.concat(item.artwork);
+  });
+  res.render('speakers', {
+      title: 'Speakers',
+      artwork : myArtWork,
+      artists : myArtists,
+      page : 'artistList'
+  });
+});
+
+
+router.get('/speakers/:speakerid', function(req, res) {
+  var myArtWork = [];
+  var myArtists = [];
+
+  appdata.speakers.forEach(function(item){
+    if(item.shortname == req.params.speakerid){
+     myArtWork = myArtWork.concat(item.artwork);
+     myArtists.push(item);
+    }
+ });
+  res.render('speakers', {
+      title: 'Speakers',
+      artwork : myArtWork,
+      artists : myArtists,
+      page : 'artistDetail'
   });
 });
 
